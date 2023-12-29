b51c64f38cdccb0e36f6c7f416cf44e1bbc59e12
Werllen Gomes <werllen.gomes@agoraa.local>
2023-12-29 08:42:24 -0300

    Commit automatico

---
+++ daily_commit.bat
@@ -0,0 +1,7 @@
+git add nfs_recebidas.xlsx
+git add recebimento_do_dia.xlsx
+git add recebimentos.py
+git add requirements.txt
+git add daily_commit.bat
+git commit -m "Commit automatico"
+git push dash_notasfiscais main
